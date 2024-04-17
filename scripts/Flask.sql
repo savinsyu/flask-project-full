50155c01fdd935408fe255254f48c3c0ad630d83
s.savin <s.savin@plagate.ru>
2024-04-17 17:48:19 +0300

    Update Flask

--- scripts/Flask.sql
+++ scripts/Flask.sql
@@ -9,6 +9,10 @@ INSERT INTO  git_and_bash (command, name) VALUES ('test', 'test')
 
 SELECT * FROM git_and_bash gab ORDER BY id DESC 
 
+SELECT * FROM git_and_bash WHERE command LIKE '%ls%' 
+
+INSERT INTO  git_and_bash (command, name) VALUES ('ls -l', 'Выводит списокм папки и файлы текущего раздела с датой изменения, размером и правами доступа')
+
 DELETE FROM git_and_bash WHERE id = 45
 
 CREATE TABLE
