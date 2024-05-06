8d7ca18e01b5a57c563467f7586439dbcedbd910
s.savin <s.savin@plagate.ru>
2024-05-06 09:51:52 +0300

    Add comments in bash script

--- bash.sh
+++ bash.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-# Скрипт запускает сначала дамп базы данных, потом копирует запросы SQL, запускает приложение и открывает адрес приложения в браузере.
+# Запускаем скрпит дампа базы данных
 venv/scripts/python.exe dump.py &&
-cp /p/s.savin/Work/dbeaver/workspace6/General1/Scripts/Flask.sql /p/s.savin/flask_project/ &&
+# Копируем скрипты sql в папку sql-scripts из рабочего пространства dbeaver
+cp /p/s.savin/Work/dbeaver/workspace6/General1/Scripts/Flask.sql /p/s.savin/flask_project/sql-scripts &&
+# Запускаем само приложение
 venv/scripts/python.exe app.py &
+# Приложение открывается в браузере по умолчанию
 venv/scripts/python.exe -m webbrowser http://127.0.0.1:82
