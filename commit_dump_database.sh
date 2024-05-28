3a984dd38d92c1315c748d9a0272c4e9f8c13696
s.savin <s.savin@plagate.ru>
2024-05-28 13:25:00 +0300

    Add commit date: Tue May 28 13:24:57 RTZ 2024

--- commit_dump_database.sh
+++ commit_dump_database.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ~/AppData/Local/Programs/Python/Python312/python.exe dump.py &
 DATE=$(date)
-TEXT="Add commit database dump$DATE"
+TEXT="Add commit database dump $DATE"
 git add dump
 git commit -m "$TEXT"
 git push
